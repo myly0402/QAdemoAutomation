8d52a233e885534ae04f5ca18207d421b6147e1c
Ly Do <mylydo.qt@gmail.com>
2024-07-05 21:14:37 +0700

    Update edge driver

--- src/test/java/utils/WebDriverUtils.java
+++ src/test/java/utils/WebDriverUtils.java
@@ -4,17 +4,23 @@ import java.util.Properties;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 
 public class WebDriverUtils {
     public static WebDriver getDriver() {
         Properties properties = PropertiesLoader.loadProperties("config.properties");
         String browser = properties.getProperty("browser");
+        String driverPath;
         switch (browser) {
+
             case "chrome":
-                String driverPath = properties.getProperty("webdriver.chrome.driver");
+                driverPath = properties.getProperty("webdriver.chrome.driver");
                 System.setProperty("webdriver.chrome.driver", driverPath);
                 return new ChromeDriver();
-
+            case "edge":
+                driverPath = properties.getProperty("webdriver.edge.driver");
+                System.setProperty("webdriver.edge.driver", driverPath);
+                return new EdgeDriver();
             default:
                 throw new IllegalArgumentException("Property 'browser' hasn't set properly");
 
